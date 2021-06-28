d965ddecb583891ac20d294cae3594b0a54483ca
SergeyMaleev <72796049+SergeyMaleev@users.noreply.github.com>
2021-06-28 20:42:43 +0400

    Добавлен базовый контроллер для EF

---
+++ ABTestReal.TestTask.Controller/Controllers/Base/EntityController.cs
@@ -0,0 +1,98 @@
+﻿using ABTestReal.TestTask.DAL.Entities;
+using ABTestReal.TestTask.Service.Reposirories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ABTestReal.TestTask.Controller.Controllers.Base
+{    
+    [ApiController]
+    public abstract class EntityController<T> : ControllerBase where T : Entity
+    {
+        private readonly IRepository<T> _repository;
+
+        protected EntityController(IRepository<T> repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetUsers()
+        {
+            return Ok(await _repository.GetAll());
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            var item = await _repository.GetById(id);
+
+            if (item is not null)
+            {
+                return Ok(item);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> Add(T item)
+        {
+            var result = await _repository.Add(item);
+
+            return CreatedAtAction(nameof(GetUser), new { id = result.Id }, result);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Update(T item)
+        {
+            var result = await _repository.Update(item);
+
+            if (result is null)
+            {
+                return NotFound(item);
+            }
+
+            return AcceptedAtAction(nameof(GetUser), new { id = result.Id }, result);
+        }
+
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Delete(T item)
+        {
+            var result = await _repository.Delete(item);
+
+            if (result is null)
+            {
+                return NotFound(item);
+            }
+
+            return Ok(result);
+        }
+
+        [HttpDelete("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _repository.DeleteById(id);
+
+            if (result is null)
+            {
+                return NotFound(id);
+            }
+
+            return Ok(result);
+        }
+    }
+}
--- ABTestReal.TestTask.Controller/Controllers/UsersController.cs
+++ ABTestReal.TestTask.Controller/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using ABTestReal.TestTask.DAL.Entities;
+﻿using ABTestReal.TestTask.Controller.Controllers.Base;
+using ABTestReal.TestTask.DAL.Entities;
 using ABTestReal.TestTask.Service.Reposirories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -11,91 +12,11 @@ namespace ABTestReal.TestTask.Controller.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class UsersController : ControllerBase
+    public class UsersController : EntityController<UserEntity>
     {
-        private readonly IRepository<UserEntity> _repository;
 
-        public UsersController(IRepository<UserEntity> repository)
-        {
-            _repository = repository;
-        }
-
-        [HttpGet]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<IActionResult> GetUsers()
-        {
-           return Ok( await _repository.GetAll());
-        }
-
-        [HttpGet("{id:int}")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> GetUser(int id)
-        {
-            var item = await _repository.GetById(id);
-
-            if (item is not null)
-            {
-                return Ok(item);
-            }
-
-            return NotFound();
-        }
-
-        [HttpPost]
-        [ProducesResponseType(StatusCodes.Status201Created)]       
-        public async Task<IActionResult> Add(UserEntity item)
-        {
-            var result = await _repository.Add(item);
-
-            return CreatedAtAction(nameof(GetUser), new { id = result.Id }, result);
-        }
-
-        [HttpPut]
-        [ProducesResponseType(StatusCodes.Status202Accepted)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> Update(UserEntity item)
-        {
-            var result = await _repository.Update(item);
-
-            if (result is null)
-            {
-                return NotFound(item);
-            }
-
-            return AcceptedAtAction(nameof(GetUser), new { id = result.Id }, result);
-        }
-
-        [HttpDelete]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> Delete(UserEntity item)
-        {
-            var result = await _repository.Delete(item);
-
-            if (result is null)
-            {
-                return NotFound(item);
-            }
-
-            return Ok(result);
-        }
-
-        [HttpDelete("{id:int}")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> Delete(int id)
-        {
-            var result = await _repository.DeleteById(id);
-
-            if (result is null)
-            {
-                return NotFound(id);
-            }
-
-            return Ok(result);
-        }
-       
-
+        public UsersController(IRepository<UserEntity> repository) : base(repository) { }
+        
+     
     }
 }
